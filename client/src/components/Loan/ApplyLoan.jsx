c090471702557bb20b9e19671c560da8edf6db24
Vinayak Sharma <vinayaksh8432@gmail.com>
2025-04-23 10:48:23 +0530

    import fix

--- client/src/components/Loan/ApplyLoan.jsx
+++ client/src/components/Loan/ApplyLoan.jsx
@@ -11,12 +11,11 @@ import { FaRegAddressCard } from "react-icons/fa6";
 import { MdOutlineHomeWork } from "react-icons/md";
 import { PiBuildingOffice } from "react-icons/pi";
 import { BsFileEarmarkText } from "react-icons/bs";
-// Updated imports to use standard @ alias that matches jsconfig.json
-import PersonalDetails from "@/components/Loan/components/PersonalDetails.jsx";
-import IdentityDetails from "@/components/Loan/components/IdentityDetails.jsx";
-import AddressDetails from "@/components/Loan/components/AddressDetails.jsx";
-import EmployeeDetails from "@/components/Loan/components/EmployeeDetails.jsx";
-import LoanAmount from "@/components/Loan/components/LoanAmount.jsx";
+import PersonalDetails from "./components/PersonalDetails";
+import IdentityDetails from "./components/IdentityDetails";
+import AddressDetails from "./components/AddressDetails";
+import EmployeeDetails from "./components/EmployeeDetails";
+import LoanAmount from "./components/LoanAmount";
 import {
     LoanApplicationProvider,
     LoanApplicationContext,
