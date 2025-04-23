89b00c8e24c6d740feb744290bf8092752b2b2ba
Vinayak Sharma <vinayaksh8432@gmail.com>
2025-04-23 11:06:51 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,12 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
-        },
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-    },
-    base: "./",
-    build: {
-        outDir: "dist",
-        assetsDir: "assets",
-        sourcemap: false,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ["react", "react-dom", "react-router-dom"],
-                },
-            },
         },
     },
 });
